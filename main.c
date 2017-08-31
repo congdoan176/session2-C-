49b7a0fa7c4fe0c620ae24a416a4cdd74aceb9e2
congdoan176 <doanncd00530@fpt.edu.vn>
2017-08-31 20:37:46 +0700

    Them moi file.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("%i", plus(5, 8));
+}
+// cộng hai số với tham số đầu vào
+// là a, b là hai số nguyên
+int plus(int a, int b){
+    int c = a + b;
+    return a+ b;
+}
